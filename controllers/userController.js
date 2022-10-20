dca73212e08ac0e9e1b7c7469863b1ddfeffb25e
Arely Rivera Gonzalez <arelyrivera1996@gmail.com>
2022-10-19 17:08:33 -0700

    fixed thoughtController and userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -40,7 +40,7 @@ module.exports = {
     User.findOneAndRemove({ _id: req.params.userId })
       .then((user) =>
         !user
-          ? res.status(404).json({ message: "No such user exists" })
+          ? res.status(404).json({ message: "Alas, no such user exists" })
           : Thought.deleteMany({
               _id: {
                 $in: user.thoughts,
@@ -96,7 +96,7 @@ module.exports = {
   removeFriend(req, res) {
     User.findOneAndUpdate(
       { _id: req.params.userId },
-      { $pull: { friends:  req.params.friendsId  } }, //?
+      { $pull: { friends:  req.params.friendsId  } }, 
       { runValidators: true, new: true }
     )
       .then((user) =>
